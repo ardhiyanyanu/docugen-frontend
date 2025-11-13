b3e6925fea3ec0a49eac40f81895c374fdaa3afa
ardhiyanyanu <ardhiyan.syahrullah@gmail.com>
2025-11-13 13:40:06 +0700

    fix: deployment error

--- lib/cognito-config.ts
+++ lib/cognito-config.ts
@@ -5,7 +5,7 @@ export const cognitoConfig: ResourcesConfig = {
     Cognito: {
       userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || '',
       userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || '',
-      identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID,
+      identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID || '',
       loginWith: {
         email: true,
       },
